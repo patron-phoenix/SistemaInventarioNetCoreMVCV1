682970d7bc8bbf2e727d66254671628277c1ca19
Ivan S <patron.phoenix@gmail.com>
2024-11-25 17:24:31 -0400

    Diseño página principal

--- SistemaInventarioNetCoreV1/Areas/Inventario/Controllers/HomeController.cs
+++ SistemaInventarioNetCoreV1/Areas/Inventario/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using SistemaInventarioNetCoreV1.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventarioNetCoreV1.Modelos;
 using SistemaInventarioNetCoreV1.Modelos.ViewModels;
 using System.Diagnostics;
 
@@ -8,15 +10,17 @@ namespace SistemaInventarioNetCoreV1.Areas.Inventario.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUnidadTrabajo _unidadTrabajo;  
+        public HomeController(ILogger<HomeController> logger, IUnidadTrabajo unidadTrabajo)
         {
             _logger = logger;
+            _unidadTrabajo = unidadTrabajo;
         }
 
-        public IActionResult Index()
+        public async Task< IActionResult> Index()
         {
-            return View();
+            IEnumerable<Producto> productoLista = await _unidadTrabajo.Producto.ObtenerTodos();
+            return View(productoLista);
         }
 
         public IActionResult Privacy()
